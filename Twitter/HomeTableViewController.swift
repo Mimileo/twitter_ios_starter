5f968fdc7493a8633c2dd5644a64c81507653d9d
Mimi Leon <mireyaleon76@gmail.com>
2020-10-01 15:24:59 -0700

    part 2 implementations

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -12,19 +12,23 @@ class HomeTableViewController: UITableViewController {
 
     var tweetArray = [NSDictionary]()
     var numberOfTweets: Int!
-    
+    //let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(loadTweets), userInfo: nil, repeats: true)
+
     let myRefreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
      
         // when view loads, load tweets
-        loadTweets()
-        self.tableView.estimatedRowHeight = 200;
+        
+        //self.tableView.estimatedRowHeight = 200;
         //self.tableView.rowHeight = UITableView.automaticDimension;
         
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
-        tableView.refreshControl = myRefreshControl
+        //Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(loadTweets), userInfo: nil, repeats: true)
+        self.tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -32,6 +36,10 @@ class HomeTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
     
     @objc func loadTweets() {
         numberOfTweets = 20
@@ -116,10 +124,14 @@ class HomeTableViewController: UITableViewController {
         //set the image
         cell.profileImageView.layer.borderWidth = 1.0
         cell.profileImageView.layer.masksToBounds = false
+        //cell.profileImageView.bounds.size = CGSize(width: 100, height: 100)
         cell.profileImageView.layer.borderColor = UIColor.white.cgColor
         cell.profileImageView.layer.cornerRadius = cell.profileImageView.frame.size.width / 2
+            //cell.profileImageView.frame.size.width / 2
+
         cell.profileImageView.clipsToBounds = true
         
+        tableView.deselectRow(at: indexPath, animated: true)
         let profileImageurl = (user["profile_image_url_https"] as? String)!
         let start = profileImageurl.startIndex
         let end = profileImageurl.index(profileImageurl.endIndex, offsetBy: -10)
@@ -133,7 +145,10 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
-        
+        cell.setFavorited(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        //cell.isSelected = false
         return cell
     }
     
@@ -146,6 +161,8 @@ class HomeTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return tweetArray.count
     }
+    
+   
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
